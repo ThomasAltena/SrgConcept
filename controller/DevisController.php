7e780d4095d389daa021eacf8164c2b904950e90
kimmg <malorygarrick@gmail.com>
2019-03-06 17:03:40 +0100

    -> Ajout de creation pdf devis au devis controlleur -> Utilisation de module lazy loading auto load pour meilleurs performances -> Refactorisation general

--- controller/DevisController.php
+++ controller/DevisController.php
@@ -4,14 +4,18 @@ if (empty($_SESSION)) {
   header('location:index.php');
 }
 
-require('../model/DevisClass.php');
-require('../model/DevisManager.php');
+use Spipu\Html2Pdf\Html2Pdf;
+use Spipu\Html2Pdf\Exception\Html2PdfException;
+use Spipu\Html2Pdf\Exception\ExceptionFormatter;
 
-require('../model/LigneDevisClass.php');
-require('../model/LigneDevisManager.php');
+spl_autoload_register(function ($class_name) {
+  if(strpos($class_name, 'Manager')){
+    include '../model/'. $class_name . '.php';
+  } else {
+    include '../model/'. $class_name . 'Class.php';
+  }
+});
 
-require('../model/OptionLigneClass.php');
-require('../model/OptionLigneManager.php');
 
 try {
   $bdd = new PDO('mysql:host=localhost;dbname=srg', 'root', '');
@@ -19,44 +23,63 @@ try {
   die('Erreur : ' . $e->getMessage());
 }
 
-$aResult = array();
+$result = array();
 $input = file_get_contents('php://input');
 $arguments = null;
 
-if( !isset($_GET['functionname']) ) { $aResult['error'] = 'No function name!'; }
+if( !isset($_GET['functionname']) ) { $result['error'] = 'No function name!'; }
 
 
-if( !isset($aResult['error']) ) {
+if( !isset($result['error']) ) {
   switch($_GET['functionname']) {
     case 'AddLignesDevis':
-    PostLignesDevis();
-    break;
+      PostLignesDevis();
+      echo json_encode($result);
+      break;
     case 'GetAllDevisInfo':
-    GetAllDevisInfo();
-    break;
+      if( !isset($_GET['devisId']) ) {
+        $result['error'] .= 'No devis Id! ';
+      } else {
+        $result['data'] = GetAllDevisInfo($_GET['devisId']);
+      }
+      echo json_encode($result);
+      break;
+    case 'GenerateDevisPDF':
+      include '../vendor/autoload.php';
+      if( !isset($_GET['devisId']) ) {
+        $result['error'] .= 'No devis Id! ';
+      } else {
+        $result['data'] = GenerateDevisPDF(($_GET['devisId']));
+      }
+      if(!isset($result['error'])){
+        echo json_encode($result['data']);
+      } else {
+        echo json_encode($result);
+      }
+      break;
     default:
-    $aResult['error'] = 'Not found function '.$_GET['functionname'].'!';
+      $result['error'] = 'Not found function '.$_GET['functionname'].'!';
+      echo json_encode($result);
     break;
   }
-  echo json_encode($aResult);
 }
 
 function PostLignesDevis() {
-  global $_SESSION, $input, $aResult, $arguments, $bdd;
+  global $_SESSION, $input, $result, $arguments, $bdd;
   if( !isset($input) ) {
-    $aResult['error'] = 'No function arguments!';
+    $result['error'] = 'No function arguments!';
   } else {
     $arguments = json_decode($input);
     if( !is_array($arguments) || (count($arguments) < 5) ) {
-      $aResult['error'] = 'Erreur - manque donnees devis!';
+      $result['error'] = 'Erreur - manque donnees devis!';
     }
   }
 
-  if( !isset($aResult['error']) ) {
+  if( !isset($result['error']) ) {
     $lignes = $arguments[4];
 
     if( !is_array($lignes) || (count($lignes) < 1) ) {
-      $aResult['error'] = 'Erreur - manque données pieces!!';
+      $result['error'] = 'Erreur - manque données pieces!!';
     } else {
       $idUser = $_SESSION['Id_user'];
       $date = date("Y-m-d");
@@ -76,7 +99,7 @@ function PostLignesDevis() {
       $DevisManager = new DevisManager($bdd); //Connexion a la BDD
       $devisInsertResult = $DevisManager->AddDevis($devisModel);
 
-      $aResult['devisResults'] = [
+      $result['devisResults'] = [
         "insertResult" => $devisInsertResult,
         "updateResult" => [],
         "devis" => $devis,
@@ -84,7 +107,7 @@ function PostLignesDevis() {
       ];
 
       if(!$devisInsertResult[0]){
-        $aResult['error'] = 'Erreur INSERT de DEVIS - '.$devisInsertResult[1][2];
+        $result['error'] = 'Erreur INSERT de DEVIS - '.$devisInsertResult[1][2];
       } else {
         $devisInsertId = $bdd->lastInsertId();
         $LigneDevisManager = new LigneDevisManager($bdd); //Connexion a la BDD
@@ -105,7 +128,7 @@ function PostLignesDevis() {
         $fichier_full_path = $upload_dir . $fichier_nom;
         $devisUpdateResult = $DevisManager->UpdateCheminSchema($devisInsertId, $fichier_full_path);
 
-        $aResult['devisResults']['updateResult'] = $devisUpdateResult;
+        $result['devisResults']['updateResult'] = $devisUpdateResult;
 
         foreach ($lignes as $ligne){
           $options = $ligne->options;
@@ -136,7 +159,7 @@ function PostLignesDevis() {
           ];
 
           if(!$ligneInsertResult[0]){
-            $aResult['error'] = 'Erreur INSERT de LIGNE - '.$ligneInsertResult[1][2];
+            $result['error'] = 'Erreur INSERT de LIGNE - '.$ligneInsertResult[1][2];
             break;
           } else {
             if( is_array($options) && (count($options) > 0) ) {
@@ -159,152 +182,328 @@ function PostLignesDevis() {
                 array_push($ligneResultObject['optionResults'], $optionResultObject);
 
                 if(!$optionLigneInsertResult[0]){
-                  $aResult['error'] = 'Erreur INSERT de OPTION_LIGNE - '.$optionLigneInsertResult[1][2];
+                  $result['error'] = 'Erreur INSERT de OPTION_LIGNE - '.$optionLigneInsertResult[1][2];
                   break 2;
                 }
               }
             }
           }
-          array_push($aResult['devisResults']['ligneResults'], $ligneResultObject);
+          array_push($result['devisResults']['ligneResults'], $ligneResultObject);
         }
       }
     }
   }
 }
 
-function GetAllDevisInfo(){
-  global $_SESSION, $input, $aResult, $arguments, $bdd;
-  if( !isset($_GET['devisId']) ) {
-    $aResult['error'] .= 'No devis Id! ';
-  }
-  if( !isset($aResult['error']) ) {
-    $devisId = $_GET['devisId'];
-    $optionsLignes = array();
-    $devi = null;
+function GetAllDevisInfo($deviId){
+  global $_SESSION, $input, $result, $arguments, $bdd;
+  $optionsLignes = array();
+  $devi = null;
 
-    $syntax = 'SELECT * FROM devis WHERE Id_devis = :iddevis';
+  $syntax = 'SELECT * FROM devis WHERE Id_devis = :iddevis';
+  $query = $bdd->prepare($syntax);
+  $query->bindValue(':iddevis', $deviId);
+
+  if(!$query->execute()) {
+    $result['error'] .= $query->errorInfo();
+  } else {
+    while ($deviResult = $query->fetch(PDO::FETCH_ASSOC))
+    {
+      $deviModel = new Devis($deviResult);
+      $devi = $deviResult;
+    }
+  }
+
+  if( !isset($result['error']) ) {
+    $devi['lignes'] = array();
+
+    $syntax = 'SELECT * FROM lignes_devis WHERE IdDevis_ligne = :iddevis';
     $query = $bdd->prepare($syntax);
-    $query->bindValue(':iddevis', $devisId);
+    $query->bindValue(':iddevis', $deviId);
 
     if(!$query->execute()) {
-      $aResult['error'] .= $query->errorInfo();
+      $result['error'] .= $query->errorInfo();
     } else {
-      while ($devi1 = $query->fetch(PDO::FETCH_ASSOC))
+      while ($ligne = $query->fetch(PDO::FETCH_ASSOC))
       {
-        $deviModel = new Devis($devi1);
-        $devi = $devi1;
+        array_push($devi['lignes'], $ligne);
       }
     }
+  }
 
-    if( !isset($aResult['error']) ) {
-      $devi['lignes'] = array();
+  if( !isset($result['error']) ) {
+    foreach ($devi['lignes'] as &$ligne){
+      $ligneModel = new LigneDevis($ligne);
 
-      $syntax = 'SELECT * FROM lignes_devis WHERE IdDevis_ligne = :iddevis';
+      $syntax = 'SELECT * FROM pieces WHERE Id_piece = :idpiece';
       $query = $bdd->prepare($syntax);
-      $query->bindValue(':iddevis', $devisId);
+      $query->bindValue(':idpiece', $ligneModel->GetIdPiece());
 
       if(!$query->execute()) {
-        $aResult['error'] .= $query->errorInfo();
+        $result['error'] .= $query->errorInfo();
+        break;
       } else {
-        while ($ligne = $query->fetch(PDO::FETCH_ASSOC))
+        while ($piece = $query->fetch(PDO::FETCH_ASSOC))
         {
-          array_push($devi['lignes'], $ligne);
+          $ligne['piece'] = $piece;
         }
       }
-    }
 
-    if( !isset($aResult['error']) ) {
-      foreach ($devi['lignes'] as &$ligne){
-        $ligneModel = new LigneDevis($ligne);
 
-        $syntax = 'SELECT * FROM pieces WHERE Id_piece = :idpiece';
+      $ligne['$options'] = array();
+      $optionsLignes = [];
+
+      $syntax = 'SELECT * FROM options_lignes WHERE IdLigne_OptionsLignes = :idligne';
+      $query = $bdd->prepare($syntax);
+      $query->bindValue(':idligne', $ligneModel->GetId());
+
+      if(!$query->execute()) {
+        $result['error'] .= $query->errorInfo();
+        break;
+      } else {
+        while ($optionLigne = $query->fetch(PDO::FETCH_ASSOC))
+        {
+          array_push($optionsLignes, $optionLigne);
+        }
+      }
+
+      foreach ($optionsLignes as $optionLigne) {
+        $optionLigneModel = new OptionLigne($optionLigne);
+
+        $syntax = 'SELECT * FROM options WHERE Id_option = :idoption';
         $query = $bdd->prepare($syntax);
-        $query->bindValue(':idpiece', $ligneModel->GetIdPiece());
+        $query->bindValue(':idoption', $optionLigneModel->GetIdOption());
 
         if(!$query->execute()) {
-          $aResult['error'] .= $query->errorInfo();
-          break;
+          $result['error'] .= $query->errorInfo();
+          break 2;
         } else {
-          while ($piece = $query->fetch(PDO::FETCH_ASSOC))
+          while ($option = $query->fetch(PDO::FETCH_ASSOC))
           {
-            $ligne['piece'] = $piece;
-          }
-        }
-
-
-        $ligne['$options'] = array();
-        $optionsLignes = [];
-
-        $syntax = 'SELECT * FROM options_lignes WHERE IdLigne_OptionsLignes = :idligne';
-        $query = $bdd->prepare($syntax);
-        $query->bindValue(':idligne', $ligneModel->GetId());
-
-        if(!$query->execute()) {
-          $aResult['error'] .= $query->errorInfo();
-          break;
-        } else {
-          while ($optionLigne = $query->fetch(PDO::FETCH_ASSOC))
-          {
-            array_push($optionsLignes, $optionLigne);
-          }
-        }
-
-        foreach ($optionsLignes as $optionLigne) {
-          $optionLigneModel = new OptionLigne($optionLigne);
-
-          $syntax = 'SELECT * FROM options WHERE Id_option = :idoption';
-          $query = $bdd->prepare($syntax);
-          $query->bindValue(':idoption', $optionLigneModel->GetIdOption());
-
-          if(!$query->execute()) {
-            $aResult['error'] .= $query->errorInfo();
-            break 2;
-          } else {
-            while ($option = $query->fetch(PDO::FETCH_ASSOC))
-            {
-              array_push($ligne['$options'], $option);
-            }
+            array_push($ligne['$options'], $option);
           }
         }
       }
     }
+  }
 
-    if( !isset($aResult['error']) ) {
-      $syntax = 'SELECT * FROM clients WHERE Id_client = :idclient';
-      $query = $bdd->prepare($syntax);
-      $query->bindValue(':idclient', $deviModel->GetIdClient());
+  if( !isset($result['error']) ) {
+    $syntax = 'SELECT * FROM clients WHERE Id_client = :idclient';
+    $query = $bdd->prepare($syntax);
+    $query->bindValue(':idclient', $deviModel->GetIdClient());
 
-      if(!$query->execute()) {
-        $aResult['error'] .= $query->errorInfo();
-      } else {
-        while ($client = $query->fetch(PDO::FETCH_ASSOC))
-        {
-          $devi['client'] = $client;
-        }
+    if(!$query->execute()) {
+      $result['error'] .= $query->errorInfo();
+    } else {
+      while ($client = $query->fetch(PDO::FETCH_ASSOC))
+      {
+        $devi['client'] = $client;
       }
     }
+  }
 
-    if( !isset($aResult['error']) ) {
-      $syntax = 'SELECT * FROM matieres WHERE Id_matiere = :idmatiere';
-      $query = $bdd->prepare($syntax);
-      $query->bindValue(':idmatiere', $deviModel->GetIdMatiere());
+  if( !isset($result['error']) ) {
+    $syntax = 'SELECT * FROM matieres WHERE Id_matiere = :idmatiere';
+    $query = $bdd->prepare($syntax);
+    $query->bindValue(':idmatiere', $deviModel->GetIdMatiere());
 
-      if(!$query->execute()) {
-        $aResult['error'] .= $query->errorInfo();
-      } else {
-        while ($matiere = $query->fetch(PDO::FETCH_ASSOC))
-        {
-          $devi['matiere'] = $matiere;
-        }
+    if(!$query->execute()) {
+      $result['error'] .= $query->errorInfo();
+    } else {
+      while ($matiere = $query->fetch(PDO::FETCH_ASSOC))
+      {
+        $devi['matiere'] = $matiere;
       }
     }
+  }
 
-    if( !isset($aResult['error']) ) {
-      $aResult['data'] = $devi;
+  if( !isset($result['error']) ) {
+    $syntax = 'SELECT * FROM user WHERE Id_user = :iduser';
+    $query = $bdd->prepare($syntax);
+    $query->bindValue(':iduser', $deviModel->GetIdUser());
+
+    if(!$query->execute()) {
+      $result['error'] .= $query->errorInfo();
+    } else {
+      while ($user = $query->fetch(PDO::FETCH_ASSOC))
+      {
+        $devi['user'] = $user;
+      }
     }
+  }
+
+  if( !isset($result['error']) ) {
+    return $devi;
+  } else {
+    return 'Données no recuperable. Voir message erreur';
   }
 }
 
+function GenerateDevisPDF($deviId){
+  global $_SESSION, $input, $result, $arguments, $bdd;
+  if( !isset($result['error']) ) {
+    $dataPDF = '';
+    $devi = GetAllDevisInfo($deviId);
 
+    $deviModel = new Devis($devi);
+    $clientModel = new Client($devi['client']);
+    $matiereModel = new Matiere($devi['matiere']);
+    $userModel = new User($devi['user']);
+
+    $ligneModelItems = [];
+    foreach ($devi['lignes'] as $ligne){
+      $ligneModel = new Ligne($ligne);
+      $pieceModel = new Piece($ligne['piece']);
+      $optionModels = [];
+
+      foreach ($ligne['options'] as $option){
+        $optionModel = new Option($option);
+        array_push($optionModels, $optionModel);
+      }
+
+      $ligneModelsItem = [
+        'ligneModel' => $ligneModel,
+        'pieceModel' => $pieceModel,
+        'optionModels' => $optionModels
+      ];
+      array_push($ligneModelItems, $ligneModelsItem);
+    }
+
+    //UTILISATEUR
+    $dataPDF .= "<table><tr><td>";
+    $dataPDF .= "<strong>".$userModel->GetNom()." ".$userModel->GetPrenom(). "</strong><br>";
+    $dataPDF .= $userModel->GetAdresse();
+    $dataPDF .= "<strong>SIRET:".$userModel->GetSiret()."</strong><br>";
+    $dataPDF .= "</td></tr></table>";
+
+    //CLIENT
+    $dataPDF .= "<table>";
+    $dataPDF .= "<tr>";
+
+    $dataPDF .= "<td><h2>Devis N°".$deviId."</h2></td></tr>";
+    $dataPDF .= "<tr>";
+    $dataPDF .= "<td >Emis le ".date('d/m/y')."<br></td></tr>";
+
+    $dataPDF .= "<tr>";
+    $dataPDF .= "<td>Par ".$clientModel->GetNom()." ".$clientModel->GetPrenom()."<br></td></tr>";
+
+    $dataPDF .= "<tr>";
+    $dataPDF .= "<td>".$clientModel->GetAdresse()."<br></td></tr>";
+
+    $dataPDF .= "<tr>";
+    $dataPDF .= "<td>".$clientModel->GetVille()."<br></td></tr>";
+
+    $dataPDF .= "<tr>";
+    $dataPDF .= "<td>".$clientModel->GetCodePostal()."<br></td></tr>";
+
+    $dataPDF .= "<tr>";
+    $dataPDF .= "<td>".$clientModel->GetTel()."</td></tr>";
+
+    $dataPDF .= "<tr><td>".$clientModel->GetMail()."</td></tr>";
+    $dataPDF .= "</table>";
+
+    //DEVIS
+    $dataPDF .= "<div>";
+    $dataPDF .= "<table  style='border: 1px solid ; width: 100%; '>";
+    $dataPDF .= "<thead>";
+    $dataPDF .= "<tr>";
+    $dataPDF .= "<th style='width: 50%';>Ref.</th>";
+    $dataPDF .= "<th style='width: 10%';>Prix HT</th>";
+    $dataPDF .= "<th style='width: 10%';>Remise</th>";
+    $dataPDF .= "<th style='width: 10%';>Prix NET</th>";
+    $dataPDF .= "<th style='width: 10%';>Prix TTC</th>";
+    $dataPDF .= "</tr>";
+    $dataPDF .= "</thead>";
+    $dataPDF .= "<tbody style='height: 100%';>";
+
+    $sommeTtc = 0;
+    $sommeNetSansRemise = 0;
+    $sommeNet = 0;
+    foreach ($ligneModelItems as $ligneModelItem) {
+      $ligneModel = $ligneModelItem['ligneModel'];
+      $pieceModel = $ligneModelItem['pieceModel'];
+      $optionModels = $ligneModelItem['optionModels'];
+
+      $prixMatiereTonne = $matiereModel.GetPrix();
+
+      $masseVolumique = 2700;
+      $volumeCM3 = $largeur * $hauteur * $profondeur;
+      $volumeM3 = $volumeCM3 /1000000;
+      $masseKG = $volumeM3 * $masseVolumique;
+      $masseTonne = $masseKG/1000;
+      $prix = $masseTonne * $prixMatiereTonne;
+
+      $pourcentageRemise = $ligneModel->GetRemise();
+      $valeurRemise = $prix * ($pourcentageRemise / 100);
+      $net = $prix - $valeurRemise;
+
+      $pourcentageTva = 20;
+      $valeurTva = $net * ($pourcentageTva / 100);
+
+      $ttc = $net + $valeurTva;
+
+      $sommeNetSansRemise += $prix;
+      $sommeNet += $net;
+      $sommeTtc += $ttc;
+
+      $dataPDF .= "<tr style='height: 100%';>";
+      $dataPDF .= "<td>".$pieceModel->GetCode()." ".$pieceModel->GetCodeSs()." ".$pieceModel->GetCodeFamille()."</td>";
+      $dataPDF .= "<td>".$prix." €</td>";
+      $dataPDF .= "<td>".$pourcentageRemise." % -".$valeurRemise."€</td>";
+      $dataPDF .= "<td>".$net." €</td>";
+      $dataPDF .= "<td>".$ttc." €</td>";
+      $dataPDF .= "</tr>";
+    }
+
+    $sommeTva = $sommeTtc - $sommeNet;
+    $sommeRemise = $sommeNetSansRemise - $sommeNet;
+
+    $dataPDF .= "</tbody>";
+    $dataPDF .= "</table>";
+    $dataPDF .= "<br>";
+
+    $dataPDF .= "<table align=right style='border: 1px solid; border-radius: 12px;'>";
+    $dataPDF .= "<tr>";
+    $dataPDF .= "<th>Total</th>";
+    $dataPDF .= "<td>".$sommeNetSansRemise." €</td>";
+    $dataPDF .= "</tr>";
+    $dataPDF .= "<tr>";
+    $dataPDF .= "<th>Remise</th>";
+    $dataPDF .= "<td> -".$sommeRemise." €</td>";
+    $dataPDF .= "</tr>";
+    $dataPDF .= "<tr>";
+    $dataPDF .= "<th>Total Net</th>";
+    $dataPDF .= "<td>".$sommeNet." €</td>";
+    $dataPDF .= "</tr>";
+    $dataPDF .= "<tr>";
+    $dataPDF .= "<th>TVA</th>";
+    $dataPDF .= "<td>".$sommeTva." €</td>";
+    $dataPDF .= "</tr>";
+    $dataPDF .= "<tr>";
+    $dataPDF .= "<th>Total TTC</th>";
+    $dataPDF .= "<td>".$sommeTtc." €</td>";
+    $dataPDF .= "</tr>";
+    $dataPDF .= "</table>";
+    $dataPDF .= "</div>";
+
+    //$content = ob_get_clean();
+    try {
+      $pdf = new HTML2PDF("p","A4","fr");
+      $pdf->pdf->SetAuthor('DOE John');
+      $pdf->pdf->SetTitle('Devis 14');
+      $pdf->pdf->SetSubject('Création d\'un Portfolio');
+      $pdf->pdf->SetKeywords('HTML2PDF, Devis, PHP');
+      $pdf->writeHTML($dataPDF);
+      ob_clean();
+      $pdf->Output('Devis.pdf');
+    } catch (HTML2PDF_exception $e) {
+      $result['error'] = $e;
+
+      //die($e);
+    }
+  } else {
+    $result['error'] = "ERREUR A PARTIR DE CREATION DEVIS PDF - ".$result['error'];
+  }
+}
 
 ?>
